4aa99425d5a4a8e3c0cb5dcea808da3ce65bc6ae
arturylab <arturylab@gmail.com>
2025-09-26 21:03:55 -0700

    Add functionality to the App and icons

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,17 @@
 import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
-import { Icon, LockIcon, MoonIcon, SunIcon } from '@/components/ui/icon';
+import { Icon, MoonIcon, SunIcon } from '@/components/ui/icon';
 import '@/global.css';
+import { useFonts } from 'expo-font';
 import { Stack } from "expo-router";
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { TouchableOpacity } from 'react-native';
+
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
 
 export default function RootLayout() {
   const [colorMode, setColorMode] = useState<'light' | 'dark'>('light');
@@ -12,6 +19,20 @@ export default function RootLayout() {
   const toggleColorMode = () => {
     setColorMode(colorMode === 'light' ? 'dark' : 'light');
   };
+
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hide();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
 
   return (
     <GluestackUIProvider mode={colorMode}>
@@ -30,36 +51,15 @@ export default function RootLayout() {
           name="index"
           options={{
             title: "Password Generator",
-            headerTitleAlign: 'left',
-            headerLeft: () => (
-              <Icon size={'xl'} className={`ml-4 ${colorMode === 'dark' ? 'white' : 'black'}`} as={LockIcon} />
-            ),
+            headerTitleAlign: 'center',
             headerRight: () => (
-              <TouchableOpacity onPress={toggleColorMode}>
+              <TouchableOpacity onPress={toggleColorMode} className='ml-4'>
                 <Icon size={'xl'} as={colorMode === 'light' ? SunIcon : MoonIcon} className='mr-4' />
               </TouchableOpacity>
             ),
           }}
         />
       </Stack>
-      
-      {/* Footer */}
-      {/* <View style={{ 
-        position: 'absolute', 
-        bottom: 0, 
-        left: 0, 
-        right: 0,
-        paddingBottom: 20,
-        paddingHorizontal: 20,
-        backgroundColor: 'transparent'
-      }}>
-        <Text 
-          size="xs" 
-          className="text-center text-gray-500"
-        >
-          © 2025 arturylab. All rights reserved.
-        </Text>
-      </View> */}
     </GluestackUIProvider>
   );
 }
